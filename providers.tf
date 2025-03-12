509d9041064948842c2029aa4957b1b471b72ab1
hirajabeen9 <hirajabeen788@gmail.com>
2025-03-12 16:44:27 +0500

    for prod env terraform for apim

--- providers.tf
+++ providers.tf
@@ -6,3 +6,12 @@ provider "azurerm" {
   client_id         = var.client_id
   client_secret     = var.client_secret
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "Practice-DevOps"
+    storage_account_name = "tfbackendstorageautomate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
