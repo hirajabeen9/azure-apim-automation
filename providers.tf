f2e64f126a75afe85de68f8bb6f6161427057ccd
hirajabeen9 <hirajabeen788@gmail.com>
2025-03-12 15:19:24 +0500

    Service Principals in GitHub Actions

--- providers.tf
+++ providers.tf
@@ -1,11 +1,5 @@
 provider "azurerm" {
   features {}
-    # Required properties for authentication
-  subscription_id = "9408054e-02d8-4db2-97d8-6625421663f4"
-}
-
-provider "azurerm" {
-  features {}
 
   subscription_id   = var.subscription_id
   tenant_id         = var.tenant_id
